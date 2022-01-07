e21bef90f603de1e073ae4a06fe7b0cff062b46a
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-01-07 14:15:00 +0800

    init 04_Choropleth-Map

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import ChoroplethMap from "./components/ChoroplethMap";
+
+const AppContainer = styled.div`
+  background-repeat: repeat;
+  background-color: blue;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  min-width: 100vw;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <ChoroplethMap />
+    </AppContainer>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "sanitize.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/typography.css";
+import "animate.css";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
